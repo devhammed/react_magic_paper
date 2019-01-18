676b74c8de848b1e14a44f6187d070776fcb2501
Oyedele Hammed <itz.harmid@gmail.com>
2019-01-18 09:22:42 +0100

    Updates

---
+++ precache-manifest.4b1fac91cadb272bdca9730f6cc238de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac0f6afd6afa184d1f85731a7a477d2",
+    "url": "/react_magic_paper/static/media/orb.5ac0f6af.png"
+  },
+  {
+    "revision": "8ea3af3a29644e3556fa",
+    "url": "/react_magic_paper/static/js/runtime~main.8ea3af3a.js"
+  },
+  {
+    "revision": "fed0816c95ccc9fec920",
+    "url": "/react_magic_paper/static/js/main.fed0816c.chunk.js"
+  },
+  {
+    "revision": "5a9814aa02ad940c1ecc",
+    "url": "/react_magic_paper/static/js/1.5a9814aa.chunk.js"
+  },
+  {
+    "revision": "fed0816c95ccc9fec920",
+    "url": "/react_magic_paper/static/css/main.3b35579f.chunk.css"
+  },
+  {
+    "revision": "46879f021c856bc749e17cf85461e740",
+    "url": "/react_magic_paper/index.html"
+  }
+];
